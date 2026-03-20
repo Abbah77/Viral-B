62d8181f1be5be539682e8f18ae631b874299fa4
Abbah77 <yakubuyakson77@gmail.com>
2026-03-20 14:57:12 +0100

    Delete storage.py

--- storage.py
+++
@@ -1,8 +0,0 @@
-from typing import Dict, Set, List
-from .models import Comment
-
-# In-memory storage for likes (video_id -> set of user_ids)
-likes_storage: Dict[str, Set[str]] = {}
-
-# In-memory storage for comments (video_id -> list of comments)
-comments_storage: Dict[str, List[Comment]] = {}
